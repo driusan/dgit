5931b1fab2a37c0785e164fe63371c1fcb81ed4d
Dave MacFarlane <david.macfarlane2@mcgill.ca>
2016-02-04 10:15:51 -0500

    Added explanation of offset calculation for index file.

--- index.go
+++ index.go
@@ -100,11 +100,30 @@ func ReadIndexEntry(file *os.File) (*GitIndexEntry, error) {
 		if n != int(nameLength) {
 			panic("Error reading the name")
 		}
+
+		// I don't understand where this +4 comes from, but it seems to work
+		// out with how the c git implementation calculates the padding..
+		//
+		// The definition of the index file format at:
+		// https://github.com/git/git/blob/master/Documentation/technical/index-format.txt
+		// claims that there should be "1-8 nul bytes as necessary to pad the entry to a multiple of eight
+		// bytes while keeping the name NUL-terminated."
+		//
+		// The fixed size of the header is 82 bytes if you add up all the types.
+		// the length of the name is nameLength bytes, so according to the spec
+		// this *should* be 8 - ((82 + nameLength) % 8) bytes of padding.
+		// But reading existant index files, there seems to be an extra 4 bytes
+		// incorporated into the index size calculation.
+		expectedOffset := 8 - ((82 + nameLength + 4) % 8)
 		whitespace := make([]byte, 1, 1)
-		var w int
+		var w uint16
 		// Read all the whitespace that git uses for alignment.
 		for _, _ = file.Read(whitespace); whitespace[0] == 0; _, _ = file.Read(whitespace) {
 			w += 1
+		}
+
+		if w != expectedOffset {
+			panic("Read incorrect number of whitespace characters")
 		}
 		if w == 0 {
 			panic("Name was not null terminated in index")
