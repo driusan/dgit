605ffe44f22920128d462627a1418525d1e70fe1
Dave MacFarlane <driusan@gmail.com>
2016-12-17 15:56:08 -0500

    Refactor merge in terms of plumbing commands

--- merge.go
+++ merge.go
@@ -41,67 +41,50 @@ func Merge(c *Client, repo *libgit.Repository, args []string) error {
 
 	}
 
-	// If --ff-only is specified, verify it is in fact a fast-forward.
-	if *ffonly {
-		for _, commit := range commits[1:] {
-			if !head.IsAncestor(c, commit) {
-				return fmt.Errorf("Not a fast-forward commit.")
-			}
-		}
+	if len(commits) != 2 {
+		return fmt.Errorf("Only fast-forward commits are currently implemented.")
+	}
+
+	// RevParse returns ParsedRevisions, which are Commitish, but slices
+	// can't be passed in terms of interfaces without converting them
+	// first.
+	var asCommitish []Commitish
+	for _, c := range commits {
+		asCommitish = append(asCommitish, c)
+	}
+
+	// Find the mergebase
+	base, err := MergeBaseOctopus(c, asCommitish)
+	if err != nil {
+		return err
+	}
+
+	// Convert the ParsedRevisions into CommitIDs
+	baseCommit, err := base.CommitID(c)
+	if err != nil {
+		return err
+	}
+	headCommit, err := commits[0].CommitID(c)
+	if err != nil {
+		return err
+	}
+
+	// Check if it's a fast-forward commit. If the merge base is HEAD,
+	// it's a fast-forward
+	if baseCommit == headCommit {
 		// A fast-forward commit is easy, just update the ref.
 		UpdateRef(c, []string{"HEAD", commits[1].Id.String()})
 
 		// And update the index
 		ReadTree(c, []string{"HEAD"})
+
+		// then update the the working directory.
+		Checkout(c, []string{"HEAD"})
 		return nil
+	}
+	if *ffonly {
+		return fmt.Errorf("Not a fast-forward commit.")
 	}
 
 	panic("Only fast-forward commits are implemented")
-	/*
-		var commits
-		for _, commit := range commits {
-		}
-		mergebase, err := MergeBase(c, repo, commits)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("Mergebase: %v\n", mergebase)
-		return nil
-	*/
-	/*
-		if len(args) != 1 {
-			fmt.Fprintf(os.Stderr, "Usage: go-git merge branchname\n")
-			return
-		}
-		commit, err := repo.GetCommitOfBranch(args[0])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Invalid branch: %s\n", args[0])
-			return
-		}
-		head, err := c.GetHeadID()
-		if err != nil {
-			panic(err)
-		}
-
-		// The current branch is an ancestor of HEAD. This
-		// is a fast-forward.
-		if headCom, err := repo.GetCommit(head); err == nil {
-			if isAncestor(headCom, fmt.Sprintf("%s", commit.Id)) {
-				fmt.Fprintf(os.Stderr, "Already up-to-date.\n")
-				return
-			}
-		}
-
-		// Head is an ancestor of the current branch.
-		if isAncestor(commit, head) {
-			hb := c.GetHeadBranch()
-			newId := fmt.Sprintf("%s", commit.Id)
-
-			ioutil.WriteFile(".git/refs/heads/"+hb, []byte(newId), 0644)
-			resetIndexFromCommit(c, newId)
-			fmt.Printf("Hooray! A Fast forward on %s! New should should be %s\n", hb, commit.Id)
-			return
-		}
-
-		panic("Only fast forward commits are currently supported.")*/
 }
--- mergebase.go
+++ mergebase.go
@@ -10,6 +10,18 @@ import (
 var Ancestor error = errors.New("Commit is an ancestor")
 var NonAncestor error = errors.New("Commit not an ancestor")
 
+func MergeBaseOctopus(c *Client, commits []Commitish) (CommitID, error) {
+	var bestSoFar Commitish = commits[0]
+	for _, commit := range commits[1:] {
+		closest, err := NearestCommonParent(c, bestSoFar, commit)
+		if err != nil {
+			return CommitID{}, err
+		}
+		bestSoFar = closest
+	}
+	return bestSoFar.CommitID(c)
+
+}
 func MergeBase(c *Client, args []string) (CommitID, error) {
 	os.Args = append([]string{"git merge-base"}, args...)
 	octopus := flag.Bool("octopus", false, "Compute the common ancestor of all supplied commits")
@@ -35,15 +47,16 @@ func MergeBase(c *Client, args []string) (CommitID, error) {
 		if err != nil {
 			return CommitID{}, err
 		}
-		var bestSoFar Commitish = commits[0]
-		for _, commit := range commits[1:] {
-			closest, err := NearestCommonParent(c, bestSoFar, commit)
-			if err != nil {
-				return CommitID{}, err
-			}
-			bestSoFar = closest
+
+		// RevParse returns ParsedRevisions, which are Commitish, but slices
+		// can't be passed in terms of interfaces without converting them
+		// first.
+		var asCommitish []Commitish
+		for _, c := range commits {
+			asCommitish = append(asCommitish, c)
 		}
-		return bestSoFar.CommitID(c)
+
+		return MergeBaseOctopus(c, asCommitish)
 	} else {
 		panic("Only octopus and is-ancestor are currently supported")
 	}
