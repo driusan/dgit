ede9265b884c7b6d448767af6536fbd90bb2a144
Dave MacFarlane <driusan@gmail.com>
2018-01-08 01:41:57 -0500

    Implement "git add -p" option
    Add the ability to interactively stage hunks to the index with the -p
    option.

--- git/add.go
+++ git/add.go
@@ -1,7 +1,10 @@
 package git
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type AddOptions struct {
@@ -24,6 +27,41 @@ type AddOptions struct {
 
 // Add implements the "git add" plumbing command.
 func Add(c *Client, opts AddOptions, files []File) (*Index, error) {
+	if opts.Patch {
+		diffs, err := DiffFiles(c, DiffFilesOptions{}, files)
+		if err != nil {
+			return nil, err
+		}
+		var patchbuf bytes.Buffer
+		if err := GeneratePatch(c, DiffCommonOptions{Patch: true}, diffs, &patchbuf); err != nil {
+			return nil, err
+		}
+		hunks, err := splitPatch(patchbuf.String(), false)
+		if err != nil {
+			return nil, err
+		}
+		hunks, err = filterHunks("stage this hunk", hunks)
+		if err == userAborted {
+			return nil, nil
+		} else if err != nil {
+			return nil, err
+		}
+
+		patch, err := ioutil.TempFile("", "addpatch")
+		if err != nil {
+			return nil, err
+		}
+		defer os.Remove(patch.Name())
+		recombinePatch(patch, hunks)
+
+		if !opts.DryRun {
+			if err := Apply(c, ApplyOptions{Cached: true}, []File{File(patch.Name())}); err != nil {
+				return nil, err
+			}
+
+		}
+		return c.GitDir.ReadIndex()
+	}
 	if len(files) == 0 {
 		if !opts.All && !opts.Update {
 			return nil, fmt.Errorf("Nothing to add. Did you mean \"git add .\"")
