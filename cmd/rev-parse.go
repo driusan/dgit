f35c446e6e1a04fa6ec679f4bfb4fbb8d247f356
sirnewton01 <newton688@gmail.com>
2018-07-16 19:55:50 -0400

    Fix rev-parse so that it exits when it produces the usage

--- cmd/rev-parse.go
+++ cmd/rev-parse.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+        "os"
 
 	"github.com/driusan/dgit/git"
 )
@@ -9,6 +10,7 @@ import (
 func RevParse(c *git.Client, args []string) (commits []git.ParsedRevision, err2 error) {
 	if len(args) == 1 && args[0] == "--help" {
 		fmt.Printf("Usage: dgit parse-rev <commid>\n")
+                os.Exit(0)
 	}
 	return git.RevParse(c, git.RevParseOptions{}, args)
 }
