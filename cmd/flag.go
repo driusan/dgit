54b86bb2b581427347759d2b4f6320f925fe8d3c
sirnewton01 <newton688@gmail.com>
2018-07-17 20:15:52 -0400

    Fix formatting

--- cmd/flag.go
+++ cmd/flag.go
@@ -1,7 +1,7 @@
 package cmd
-  
+
 import (
-        "fmt"
+	"fmt"
 )
 
 // A string value compatible with a flag var
@@ -11,20 +11,20 @@ import (
 //  or using the original and alias then an error
 //  is raised.
 type aliasedStringValue string
- 
+
 func newAliasedStringValue(p *string, val string) *aliasedStringValue {
-        *p = val
-        return (*aliasedStringValue)(p)
+	*p = val
+	return (*aliasedStringValue)(p)
 }
- 
+
 func (s *aliasedStringValue) Set(val string) error {
-        if *s != "" {
-                return fmt.Errorf("Value already set to %v", val)
-        }
-        *s = aliasedStringValue(val)
-        return nil
+	if *s != "" {
+		return fmt.Errorf("Value already set to %v", val)
+	}
+	*s = aliasedStringValue(val)
+	return nil
 }
- 
+
 func (s *aliasedStringValue) Get() interface{} { return string(*s) }
- 
+
 func (s *aliasedStringValue) String() string { return string(*s) }
