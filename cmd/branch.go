c8a3f2f55b718b5c35ef249f8bece37a9be7555b
sirnewton01 <newton688@gmail.com>
2018-07-19 10:05:48 -0400

    Add better usage handling for branch subcommand, fix travis script to work correctly on user github repos

--- cmd/branch.go
+++ cmd/branch.go
@@ -8,12 +8,25 @@ import (
 )
 
 func Branch(c *git.Client, args []string) {
-	if len(args) == 1 && args[0] == "--help" {
+	flags := flag.NewFlagSet("branch", flag.ExitOnError)
+	flags.SetOutput(flag.CommandLine.Output())
+	flags.Usage = func() {
 		flag.Usage()
-		os.Exit(0)
+		fmt.Fprintf(flag.CommandLine.Output(), "\n\nOptions:\n")
+		flags.PrintDefaults()
 	}
 
-	switch len(args) {
+        // These flags can be moved out of these lists and below as proper flags as they are implemented
+        for _, bf := range []string {"d", "delete", "D", "create-reflog", "f", "force", "m", "move", "M", "c", "copy", "C", "no-color", "i", "ignore-case", "no-column", "r", "remotes", "a", "all", "v", "vv", "verbose", "q", "quiet", "no-abbrev", "no-track", "unset-upstream", "edit-description" } {
+                flags.Var(newNotimplBoolValue(), bf, "Not implemented")
+        }
+        for _, sf := range []string {"color", "abbrev", "column", "sort", "no-merged", "contains", "no-contains", "points-at", "format", "set-upstream-to", "u"} {
+                flags.Var(newNotimplStringValue(), sf, "Not implemented")
+        }
+       
+        flags.Parse(args)
+
+	switch flags.NArg() {
 	case 0:
 		branches, err := c.GetBranches()
 		if err != nil {
@@ -35,8 +48,8 @@ func Branch(c *git.Client, args []string) {
 			return
 		}
 		b := git.Branch(headref)
-		if err := c.CreateBranch(args[0], b); err != nil {
-			fmt.Fprintf(os.Stderr, "Could not create branch (%v): %v\n", args[0], err)
+		if err := c.CreateBranch(flags.Arg(0), b); err != nil {
+			fmt.Fprintf(os.Stderr, "Could not create branch (%v): %v\n", flags.Arg(0), err)
 		}
 	default:
 		flag.Usage()
