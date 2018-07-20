18082f92215432aa55e75670bbb4619186e3a5a7
Chris McGee <newton688@gmail.com>
2018-07-20 09:34:53 -0400

    Add file and line number traceability to logs inside tests (#60)
    Add file and line number traceability to logs inside tests, only show log if verbose is set

---
+++ cmd/main_test.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+        "flag"
+        "io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+        flag.Parse()
+        if !testing.Verbose() {
+                log.SetOutput(ioutil.Discard)
+        }
+	code := m.Run()
+	os.Exit(code)
+}
---
+++ git/main_test.go
@@ -0,0 +1,19 @@
+package git
+
+import (
+        "flag"
+        "io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+        flag.Parse()
+        if !testing.Verbose() {
+                log.SetOutput(ioutil.Discard)
+        }
+	code := m.Run()
+	os.Exit(code)
+}
