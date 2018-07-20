054af6a451b6bdb5cf38840092a887c32931a808
sirnewton01 <newton688@gmail.com>
2018-07-19 22:23:13 -0400

    Add source code file and line number traceability to logs when run in tests

---
+++ cmd/main_test.go
@@ -0,0 +1,13 @@
+package cmd
+
+import (
+        "log"
+        "os"
+        "testing"
+)
+
+func TestMain(m *testing.M) {
+    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+    code := m.Run() 
+    os.Exit(code)
+}
---
+++ git/main_test.go
@@ -0,0 +1,13 @@
+package git
+
+import (
+        "log"
+        "os"
+        "testing"
+)
+
+func TestMain(m *testing.M) {
+    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+    code := m.Run() 
+    os.Exit(code)
+}
