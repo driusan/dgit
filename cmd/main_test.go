35cdf09563e3d15c34fb70eeb6bda62b017b7f93
sirnewton01 <newton688@gmail.com>
2018-07-19 22:33:43 -0400

    Formatting

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,13 +1,13 @@
 package cmd
 
 import (
-        "log"
-        "os"
-        "testing"
+	"log"
+	"os"
+	"testing"
 )
 
 func TestMain(m *testing.M) {
-    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-    code := m.Run() 
-    os.Exit(code)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	code := m.Run()
+	os.Exit(code)
 }
