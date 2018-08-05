c1088616a09ed045f2133f7d76b72ae73bd7b474
Dave MacFarlane <driusan@gmail.com>
2018-08-05 01:54:20 -0400

    Re-fix ls-files test

--- git/lsfiles.go
+++ git/lsfiles.go
@@ -66,6 +66,14 @@ files:
 					}
 				}
 				if !dirHasTracked {
+					if opts.Directory {
+						if opts.NoEmptyDirectory {
+							if files, err := ioutil.ReadDir(fname.String()); len(files) == 0 && err == nil {
+								continue
+							}
+						}
+						indexPath += "/"
+					}
 					untracked = append(untracked, &IndexEntry{PathName: indexPath})
 					continue
 				}
@@ -299,15 +307,6 @@ func LsFiles(c *Client, opt LsFilesOptions, files []File) ([]*IndexEntry, error)
 				if skip {
 					continue
 				}
-			}
-
-			if f.IsDir() && opt.Directory {
-				if opt.NoEmptyDirectory {
-					if files, err := ioutil.ReadDir(f.String()); len(files) == 0 && err == nil {
-						continue
-					}
-				}
-				f += "/"
 			}
 
 			fs = append(fs, file)
