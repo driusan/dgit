e08464ba531fdfc89717cee727e52f8090f66ed8
Dave MacFarlane <driusan@gmail.com>
2018-08-07 14:18:12 -0400

    Improve cat-file enough that commit-tree tests pass
    This improves the handling of git cat-file enough that the
    commit-tree test (which depends on it) succeeds.
    
    cat-file is still missing enough options that the cat-file
    tests themselves fail (in particular, it does a lot of --batch
    tests and annotated tags which we don't yet support.)

--- cmd/catfile.go
+++ cmd/catfile.go
@@ -22,19 +22,41 @@ func CatFile(c *git.Client, args []string) error {
 	flags.BoolVar(&options.Pretty, "p", false, "Pretty print the object content")
 	flags.BoolVar(&options.Size, "s", false, "Print the size of the object")
 	flags.BoolVar(&options.Type, "t", false, "Print the type of the object")
+	flags.BoolVar(&options.ExitCode, "e", false, "Exit with 0 status if file exists and is valid")
+	flags.BoolVar(&options.AllowUnknownType, "allow-unknown-type", false, "Allow types that are unknown to git")
 	flags.Parse(args)
 	oargs := flags.Args()
 
-	shas, err := git.RevParse(c, git.RevParseOptions{}, oargs)
-	if err != nil {
-		return err
-	}
-	for _, s := range shas {
-		val, err := git.CatFile(c, s.Id, options)
+	switch len(oargs) {
+	case 0:
+		flags.Usage()
+		return nil
+	case 1:
+		println("I am here")
+		shas, err := git.RevParse(c, git.RevParseOptions{}, oargs)
+		if err != nil {
+			return err
+		}
+		val, err := git.CatFile(c, "", shas[0].Id, options)
 		if err != nil {
 			return err
 		}
 		fmt.Println(val)
+		return nil
+	case 2:
+		shas, err := git.RevParse(c, git.RevParseOptions{}, []string{oargs[1]})
+		if err != nil {
+			return err
+		}
+		val, err := git.CatFile(c, oargs[0], shas[0].Id, options)
+		if err != nil {
+			return err
+		}
+		fmt.Println(val)
+		return nil
+	default:
+		flags.Usage()
+
 	}
 	return nil
 }
