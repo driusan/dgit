800287e68e35112829ff0ce665695b3241c23304
Dave MacFarlane <driusan@gmail.com>
2018-08-10 21:45:33 -0400

    oops

--- git/file_inode_darwin.go
+++ git/file_inode_darwin.go
@@ -7,7 +7,7 @@ import (
 func (f File) INode() uint32 {
 	stat, err := f.Lstat()
 	if err != nil {
-		return 0, 0
+		return 0
 	}
 	rawstat := stat.Sys().(*syscall.Stat_t)
 	return uint32(rawstat.Ino)
