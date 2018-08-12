6c842bc5e9577192ca4474692c556299087a7c0e
Chris McGee <newton688@gmail.com>
2018-08-12 17:41:43 -0400

    Extract the go get tests into a shell script that is more readable

---
+++ go-get-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Running go get tests"
+
+# Keep existing state
+export ORIG_PATH=$PATH
+export ORIG_GIT=$(which git)
+
+export TEST_PKG=github.com/golang/protobuf/proto
+export TEST_GIT_DIR=../../golang/protobuf
+
+echo "Adding dgit to the path"
+go build
+mkdir bin
+cp dgit bin/git
+export PATH=$(pwd/bin):$PATH
+
+echo "Go get a package"
+go get ${TEST_PKG}
+
+echo "Reset the package back one commit from master"
+$ORIG_GIT -C ${TEST_GIT_DIR} reset HEAD^1
+$ORIG_GIT -C ${TEST_GIT_DIR} checkout .
+
+echo "Run go get -u on the package"
+go get -u github.com/golang/protobuf/proto
+
+echo "Verify that the branch is now up to date with master"
+$ORIG_GIT -C ${TEST_GIT_DIR} status | grep "Your branch is up to date with 'origin/master'."
+
+export PATH=$ORIG_PATH
