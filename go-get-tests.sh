e17ebb9e41a9103d72ff7569c0f765b122455ba3
Chris McGee <newton688@gmail.com>
2018-08-12 17:53:57 -0400

    Add more checks in the go get tests

--- go-get-tests.sh
+++ go-get-tests.sh
@@ -19,8 +19,9 @@ export DGIT_TRACE=/tmp/go-get-dgit-log.$$.txt
 
 echo "Go get a package"
 go get ${TEST_PKG}
+test -d ${TEST_GIT_DIR} || die "ERROR: Go get didn't work"
 
-test -f $DGIT_TRACE || echo "ERROR: Dgit wasn't called for the go get test"
+test -f $DGIT_TRACE || die "ERROR: Dgit wasn't called for the go get test"
 unset DGIT_TRACE
 
 echo "Reset the package back one commit from master"
@@ -31,6 +32,6 @@ echo "Run go get -u on the package"
 go get -u github.com/golang/protobuf/proto
 
 echo "Verify that the branch is now up to date with master"
-$ORIG_GIT -C ${TEST_GIT_DIR} status | grep "Your branch is up to date with 'origin/master'."
+$ORIG_GIT -C ${TEST_GIT_DIR} status | grep "Your branch is up to date with 'origin/master'." || die "ERROR: Update didn't work"
 
 export PATH=$ORIG_PATH
