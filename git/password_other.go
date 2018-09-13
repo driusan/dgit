100c0e82cfa87d6f0a18eb8d14d24e73445944b1
Dave MacFarlane <driusan@gmail.com>
2018-09-13 18:01:23 -0400

    Add factotum support for Plan 9 and disable password echo on other
    This improves the reading of passwords for git push from factotum
    on Plan 9, and without echoing to the terminal on other systems.

---
+++ git/password_other.go
@@ -0,0 +1,15 @@
+//+build !plan9
+package git
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func getPassword(url string) (string, error) {
+	fmt.Fprintf(os.Stderr, "Password: ")
+	pwb, err := terminal.ReadPassword(0)
+	return string(pwb), err
+}
