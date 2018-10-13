f6ee6a0390c579dabfbcd3ec7896e85cc6d0dd48
Dave MacFarlane <driusan@gmail.com>
2018-10-13 14:25:17 -0400

    Stupid typo

--- git/protectfs_darwin.go
+++ git/protectfs_darwin.go
@@ -2,9 +2,9 @@ package git
 
 // protectHFS defaults to true on Mac
 func protectHFS(c *Client) bool {
-	return config.GetConfig("core.protectHFS") != "false"
+	return c.GetConfig("core.protectHFS") != "false"
 }
 
 func protectNTFS(c *Client) bool {
-	return config.GetConfig("core.protectNTFS") == "true"
+	return c.GetConfig("core.protectNTFS") == "true"
 }
