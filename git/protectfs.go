39d707ce7728178acb5b23bae6dc6f53e1ed95fb
Dave MacFarlane <driusan@gmail.com>
2018-10-15 18:43:44 -0400

    Add config cache to client to improve checkout time (#161)
    Since the protectHFS and protectNTFS config options were added to dgit
    in order to pass more of the official git tests, readtree has been
    looking them up for each file.  This results in the global and local
    configs each getting parsed twice per file in the repo when doing a
    checkout.  (This is obviously particularly bad for large repos.)
    
    This adds 2 levels of caching to looking up a config option:
    
    1. The parsed config files (both local and global) are stored in the
       Client so that they only need to be parsed once per command invocation.
    2. A map is used to store values that have already been looked up
       since they shouldn't change between git invocations (with the exception
       of the "git config" command.)
    
    read-tree is currently the only thing updated to use the new
    client.GetConfig function, but it would probably be a good idea to use
    it consistently throughout dgit.

--- git/protectfs.go
+++ git/protectfs.go
@@ -4,34 +4,9 @@
 package git
 
 func protectHFS(c *Client) bool {
-	config, err := LoadLocalConfig(c)
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectHFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	config, err = LoadGlobalConfig()
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectHFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	return false
-
+	return c.GetConfig("core.protectHFS") == "true"
 }
 
 func protectNTFS(c *Client) bool {
-	config, err := LoadLocalConfig(c)
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectNTFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	config, err = LoadGlobalConfig()
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectNTFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	return false
+	return c.GetConfig("core.protectNTFS") == "true"
 }
--- git/protectfs_windows.go
+++ git/protectfs_windows.go
@@ -1,35 +1,10 @@
 package git
 
 func protectHFS(c *Client) bool {
-	config, err := LoadLocalConfig(c)
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectHFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	config, err = LoadGlobalConfig()
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectHFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	return false
-
+	return c.GetConfig("core.protectHFS") == "true"
 }
 
 // protectNTFS defaults to true on windows
 func protectNTFS(c *Client) bool {
-	config, err := LoadLocalConfig(c)
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectNTFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	config, err = LoadGlobalConfig()
-	if err == nil {
-		if cfg, _ := config.GetConfig("core.protectNTFS"); cfg != "" {
-			return cfg == "true"
-		}
-	}
-	return true
+	return c.GetConfig("core.protectNTFS") != "false"
 }
