4fa939e9e96f79e6f5341d6601cc7ad6e32b7703
Justin Overfelt <joverfelt@gmail.com>
2019-02-25 18:20:08 -0500

    Add NetBSD support for ctime and inode syscalls

---
+++ git/file_ctime_netbsd.go
@@ -0,0 +1,17 @@
+// +build netbsd
+
+package git
+
+import (
+	"syscall"
+)
+
+func (f File) CTime() (uint32, uint32) {
+	stat, err := f.Lstat()
+	if err != nil {
+		return 0, 0
+	}
+	rawstat := stat.Sys().(*syscall.Stat_t)
+	tspec := rawstat.Ctimespec
+	return uint32(tspec.Sec), uint32(tspec.Nsec)
+}
