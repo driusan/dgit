c99f29611e03445e22e54bbf9ef9d7c95aead1de
Dave MacFarlane <driusan@gmail.com>
2019-03-09 15:46:26 -0500

    Add git rev-parse --verify tests to Travis
    This adds the rev-parse-verify tests to Travis. Tests that depend
    on rev-parse supporting the '@' suffix (which looks up a rev based
    on the reflog) are ignored.

--- cmd/rev-parse.go
+++ cmd/rev-parse.go
@@ -7,10 +7,31 @@ import (
 	"github.com/driusan/dgit/git"
 )
 
-func RevParse(c *git.Client, args []string) (commits []git.ParsedRevision, err2 error) {
+func RevParse(c *git.Client, args []string) ([]git.ParsedRevision, git.RevParseOptions, error) {
+	flags := newFlagSet("rev-parse")
+	opts := git.RevParseOptions{}
+
+	flags.BoolVar(&opts.Verify, "verify", false, "Verify a single object")
+	flags.BoolVar(&opts.Quiet, "quiet", false, "Be quiet")
+	flags.BoolVar(&opts.Quiet, "q", false, "Alias of -quiet")
+	flags.StringVar(&opts.Default, "default", "", "Specify a default value if no user input provided")
+	flags.Parse(args)
+	args = flags.Args()
+
+	// --default can be specified at any arbitrary location, so we don't
+	// depend on flags.Parse() to handle it.
+	for i, arg := range args {
+		if arg == "--default" && i != len(args)-1 {
+			opts.Default = args[i+1]
+			args = append(args[0:i], args[i+2:]...)
+			break
+		}
+	}
 	if len(args) == 1 && args[0] == "--help" {
 		flag.Usage()
 		os.Exit(0)
 	}
-	return git.RevParse(c, git.RevParseOptions{}, args)
+
+	commits, err := git.RevParse(c, opts, args)
+	return commits, opts, err
 }
