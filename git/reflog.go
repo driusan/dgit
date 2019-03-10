d87ee21b381c73eab597f9db167ab1d8ee9a1a9c
Dave MacFarlane <driusan@gmail.com>
2019-03-09 20:33:56 -0500

    Fix path for reflog exists

--- git/reflog.go
+++ git/reflog.go
@@ -1,6 +1,11 @@
 package git
 
+import (
+	"path/filepath"
+)
+
 // Returns true if a reflog exists for refname r under client.
 func ReflogExists(c *Client, r Refname) bool {
-	return c.GitDir.File(File(r)).Exists()
+	path := filepath.Join(c.GitDir.String(), "logs", string(r))
+	return c.GitDir.File(File(path)).Exists()
 }
