9aba06b3bbfa7de607443a430df36561d2a7b885
Dave MacFarlane <driusan@gmail.com>
2019-03-09 22:17:23 -0500

    Manually parse args

--- cmd/rev-parse.go
+++ cmd/rev-parse.go
@@ -2,46 +2,39 @@ package cmd
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/driusan/dgit/git"
 )
 
 func RevParse(c *git.Client, args []string) ([]git.ParsedRevision, git.RevParseOptions, error) {
-	// We can't use newFlagSet because some flags are handled internally
-	// to RevParse and are context-sensitive, so we can't ExitOnError
-	// when there's an invalid flag.
-	flags := flag.NewFlagSet("rev-parse", flag.ContinueOnError)
-	flags.SetOutput(flag.CommandLine.Output())
-	flags.Usage = func() {
-		flag.Usage()
-		fmt.Fprintf(flag.CommandLine.Output(), "\n\nOptions:\n")
-		flags.PrintDefaults()
-	}
-
+	// We need to manually parse flags, because they're context sensitive.
 	opts := git.RevParseOptions{}
 
-	flags.BoolVar(&opts.Verify, "verify", false, "Verify a single object")
-	flags.BoolVar(&opts.Quiet, "quiet", false, "Be quiet")
-	flags.BoolVar(&opts.Quiet, "q", false, "Alias of -quiet")
-	flags.StringVar(&opts.Default, "default", "", "Specify a default value if no user input provided")
-	flags.Parse(args)
-	args = flags.Args()
-
-	// --default can be specified at any arbitrary location, so we don't
-	// depend on flags.Parse() to handle it.
-	for i, arg := range args {
-		if arg == "--default" && i != len(args)-1 {
-			opts.Default = args[i+1]
-			args = append(args[0:i], args[i+2:]...)
-			break
+	var parsedargs []string
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "--quiet", "-q":
+			opts.Quiet = true
+		case "--verify":
+			opts.Verify = true
+		case "--default":
+			switch i {
+			case len(args) - 1:
+				return nil, opts, fmt.Errorf("Must provide parameter for --default")
+			default:
+				opts.Default = args[i+1]
+				i++
+			}
+		case "--help":
+			flag.Usage()
+			os.Exit(0)
+		default:
+			parsedargs = append(parsedargs, args[i])
 		}
 	}
-	if len(args) == 1 && args[0] == "--help" {
-		flag.Usage()
-		os.Exit(0)
-	}
 
-	commits, err := git.RevParse(c, opts, args)
+	commits, err := git.RevParse(c, opts, parsedargs)
 	return commits, opts, err
 }
