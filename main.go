8893223ac38c42121f8d57ccb9808f8515270d24
Chris McGee <newton688@gmail.com>
2019-03-13 20:14:34 -0400

    Fix formatting

--- main.go
+++ main.go
@@ -434,8 +434,8 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(4)
 		}
-        case "for-each-ref":
-                subcommandUsage = "[<pattern>...]"
+	case "for-each-ref":
+		subcommandUsage = "[<pattern>...]"
 		if err := cmd.ForEachRef(c, args); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(4)
