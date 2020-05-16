71e989cc5c14695eb13468a013ed644133a78f84
Dave MacFarlane <driusan@gmail.com>
2020-05-16 23:24:41 +0000

    Don't spam progress on Plan9 either

--- git/progressf_plan9.go
+++ git/progressf_plan9.go
@@ -3,9 +3,20 @@ package git
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
+var lastProgress int64
+
 // Print progress information to stderr
-func progressF(fmtS string, args ...interface{}) {
+func progressF(fmtS string, done bool, args ...interface{}) {
+	if done {
+		fmt.Fprintf(os.Stderr, "\n"+fmtS+", done\n", args...)
+	}
+	now := time.Now().Unix()
+	if lastProgress > 0 && now-lastProgress < 3 {
+		return
+	}
 	fmt.Fprintf(os.Stderr, "\n"+fmtS, args...)
+	lastProgress = now
 }
